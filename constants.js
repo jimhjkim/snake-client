651cd7befdfcd90690723378fbd8541910002478
Jim Kim <jim.hyunjin.kim@gmail.com>
2019-09-25 20:44:44 +0000

    constants module

---
+++ constants.js
@@ -0,0 +1,16 @@
+const IP = '10.0.2.15';
+const PORT = 8080;
+
+const INPUT = {
+  w: 'Move: up',
+  a: 'Move: left',
+  s: 'Move: down',
+  d: 'Move: right',
+  p: 'Say: Puta!',
+  u: 'Say: U suck!'
+}
+module.exports = {
+  IP,
+  PORT,
+  INPUT
+};
--- input.js
+++ input.js
@@ -2,7 +2,8 @@
  * Setup User Interface 
  * Specifically, so that we can handle user input via stdin
  */
-// const net = require('net');
+const { INPUT }= require('./constants');
+
 let connection;
 
 const setupInput = function(conn) {
@@ -19,29 +20,32 @@ const setupInput = function(conn) {
   return stdin;
 };
 const handleUserInput = (data, conn) => {
-  switch(data) {
-    case 'w':
-      conn.write('Move: up');
-      break;
-    case 'a':
-      conn.write('Move: left');
-      break;
-    case 'd':
-      conn.write('Move: right');
-      break;
-    case 's':
-      conn.write('Move: down');
-      break;
-    case '\u0003': //-> \u0003 maps to ctrl+c input
-      console.log('Thanks for using me, ciao!');
-      process.exit();
-      break;
-    case 'p':
-      conn.write('Say: puta');
-      break;
-    case 'u':
-      conn.write('Say: u suck');
-      break;
-  }
+  if (data === '\u0003') process.exit();
+  conn.write(INPUT[data]);
+  
+  // switch(data) {
+  //   case 'w':
+  //     conn.write('Move: up');
+  //     break;
+  //   case 'a':
+  //     conn.write('Move: left');
+  //     break;
+  //   case 'd':
+  //     conn.write('Move: right');
+  //     break;
+  //   case 's':
+  //     conn.write('Move: down');
+  //     break;
+    // case '\u0003': //-> \u0003 maps to ctrl+c input
+    //   console.log('Thanks for using me, ciao!');
+    //   process.exit();
+    //   break;
+    // case 'p':
+    //   conn.write('Say: puta');
+    //   break;
+    // case 'u':
+    //   conn.write('Say: u suck');
+    //   break;
+  // }
 };
 module.exports = { setupInput };
