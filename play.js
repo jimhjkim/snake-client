723d0d6b6d11b119c0d12e5ce1ec49c21af0b130
Jim Kim <jim.hyunjin.kim@gmail.com>
2019-09-25 19:35:06 +0000

    input module

--- play.js
+++ play.js
@@ -1,28 +1,8 @@
 // play.js
 const { connect } = require('./client');
+const { setupInput } = require('./input');
+
 console.log('Connecting ...');
 connect();
 
-/**
- * Setup User Interface 
- * Specifically, so that we can handle user input via stdin
- */
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  
-  stdin.on('data', (data) => {
-    handleUserInput(data);
-  })
-  
-  return stdin;
-}
-const handleUserInput = (data) => {
-  if (data === '\u0003') { //-> \u0003 maps to ctrl+c input
-  console.log('Thanks for using me, ciao!');
-  process.exit();
-  }
-};
 setupInput();
